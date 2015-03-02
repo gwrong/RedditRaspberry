f20ad4757bf159596286677ec5c5777615e4cae4
gwrong <pi@raspberrypi.(none)>
2015-03-02 03:04:57 +0000

    Got it running on the Pi with MySql

--- src/GatherComments.py
+++ src/GatherComments.py
@@ -4,8 +4,12 @@ Created on Feb 23, 2015
 @author: Graham
 '''
 
+import MySQLdb
 import praw
 import time
+
+db = MySQLdb.connect("localhost", "root", "11rekopPoker11", "test")
+curs = db.cursor()
 
 r = praw.Reddit(user_agent='u\MoldyBrick Testing 1.0')
 r.login('MoldyBrick', 'poker11')
@@ -13,6 +17,8 @@ file = open("Comment Test.txt", "w")
 for x in range(0, 10):
     all_comments = r.get_comments('Fitness')
     for comment in all_comments:
+	curs.execute("INSERT INTO test values(%s)", (comment.body))
         file.write(comment.body + "\n") 
     time.sleep(90)
+db.commit()
 file.close()
